9c8258df23d37dd2460b4b4420c61960d02b373f
Colab bui <buichau40@gmail.com>
2024-04-15 12:43:02 +0700

    new update

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 function WishList() {
   return (
     <div className="h-[100vh] w-[100vw] flex items-center justify-center bg-white text-black text-3xl">
       <b>Tính năng đang phát triển. Vui lòng quay lại sau....☺️☺️☺️</b>
+      <Link href={"/"}>Back home!!</Link>
     </div>
   );
 }
